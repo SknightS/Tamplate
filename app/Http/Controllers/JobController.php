8ed62d56fe628e852713964e789e5d5e865f4ae1
mdsak <md.sakibrahman@hotmail.com>
2018-08-14 02:59:36 +0600

    Sakib

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -22,7 +22,7 @@ class JobController extends Controller
         $jobtype = jobtype::select('id', 'typeName')
             ->get();
 
-        $alljob = Post::select('*','job.id as jobid','jobName', 'company_branch.name as cname', 'post.description as pdes','typeName','address.addresscol as address','job.job_amount as job_amount')
+        $alljob = Post::select('*','job.id as jobid','post.id as postid','jobName', 'company_branch.name as cname', 'post.description as pdes','typeName','address.addresscol as address','job.job_amount as job_amount')
             ->leftJoin('job', 'job.id', 'post.fkjobId')
             ->leftjoin ('company_branch','job.company_branch_id','company_branch.id')
             ->leftjoin('jobtype','job.fkjobTypeId','jobtype.id')
@@ -47,7 +47,7 @@ class JobController extends Controller
     public function jobdetails($postid){
 
 
-        $jobdetails = Post::select('*','job.id as jobid','post.id as postid','jobName', 'company_branch.name as cname', 'post.description as pdes','typeName','address.addresscol as address','job.job_amount as job_amount')
+        $jobdetails = Post::select('*','job.id as jobid','post.id as postid','jobName', 'company_branch.name as cname','job.description as jdetails','company_branch.image as image ', 'post.description as pdes','typeName','address.addresscol as address','job.job_amount as job_amount', 'master_state.name as statename','master_subarb.name as cityname')
             ->leftJoin('job', 'job.id', 'post.fkjobId')
             ->leftjoin ('company_branch','job.company_branch_id','company_branch.id')
             ->leftjoin('jobtype','job.fkjobTypeId','jobtype.id')
@@ -55,8 +55,9 @@ class JobController extends Controller
             ->leftjoin('master_subarb','address.master_subarb_id','master_subarb.id')
             ->leftjoin('master_state','master_subarb.master_state_id','master_state.id')
             ->where ('post.id', $postid)
-            ->get();
-        return view('layouts.job-details', $jobdetails);
+            ->first();
+        return view('layouts.job-details')
+            ->with('jobdetails', $jobdetails);
 
     }
 }
--- resources/views/layouts/job-details.blade.php
+++ resources/views/layouts/job-details.blade.php
@@ -6,11 +6,11 @@
 			<div class="inner">
 				<div class="container-fluid">
 					<p class="breadcrumb-menu">
-						<a href="index.php"><i class="ion-ios-home"></i></a>
+						<a href="{{route('home')}}"><i class="ion-ios-home"></i></a>
 						<i class="ion-ios-arrow-right arrow-right"></i>
-						<a href="#0">Job listing - list view</a>
+						<a href="#">Job Details</a>
 					</p> <!-- end .breabdcrumb-menu -->
-					<h2 class="breadcrumb-title flex items-center">Front-end developer needed
+					<h2 class="breadcrumb-title flex items-center">{{$jobdetails->jobName}}
 						<button type="button" class="button full-time button-sm">Full time</button>
 					</h2>
 				</div> <!-- end .container-fluid -->
@@ -54,82 +54,29 @@
 
 						<div class="divider"></div>
 
-						<div class="latest-updates-widget-wrapper jobs-widget">
-							<h6>Latest updates</h6>
-							<div class="latest-update flex no-column no-wrap">
-								<div class="left-side-inner">
-									<img src="images/avatar11.jpg" alt="avatar" class="img-responsive">
-								</div> <!-- end .left-side -->
-								<div class="right-side-inner">
-									<h5><span>James Patel</span>has got a job!</h5>
-									<h5><span>Web Designer</span>for Banana Inc. in<a href="#0">Art/Design</a></h5>
-								</div> <!-- end .right-side -->
-							</div> <!-- end .latest-update -->
-
-							<div class="latest-update flex no-column no-wrap">
-								<div class="left-side-inner">
-									<img src="images/avatar12.jpg" alt="avatar" class="img-responsive">
-								</div> <!-- end .left-side -->
-								<div class="right-side-inner">
-									<h5><span>Alice Phillips</span>has got a job!</h5>
-									<h5><span>Web Designer</span>for Banana Inc. in<a href="#0">Art/Design</a></h5>
-								</div> <!-- end .right-side -->
-							</div> <!-- end .latest-update -->
-
-							<div class="latest-update flex no-column no-wrap">
-								<div class="left-side-inner">
-									<img src="images/company-logo11.jpg" alt="company-logo" class="img-responsive">
-								</div> <!-- end .left-side -->
-								<div class="right-side-inner">
-									<h5><span>Evotweet</span>has got a job!</h5>
-									<h5><a href="#0">Front-end developer</a>needed in<a href="#0">Technologies</a></h5>
-								</div> <!-- end .right-side -->
-							</div> <!-- end .latest-update -->
-
-							<div class="latest-update flex no-column no-wrap">
-								<div class="left-side-inner">
-									<img src="images/avatar13.jpg" alt="avatar" class="img-responsive">
-								</div> <!-- end .left-side -->
-								<div class="right-side-inner">
-									<h5><span>Wayne Welch</span>has got a job!</h5>
-									<h5><span>Software Engineer</span>for Apple Inc. in<a href="#0">Technologies</a></h5>
-								</div> <!-- end .right-side -->
-							</div> <!-- end .latest-update -->
-
-							<div class="latest-update flex no-column no-wrap">
-								<div class="left-side-inner">
-									<img src="images/company-logo12.jpg" alt="company-logo" class="img-responsive">
-								</div> <!-- end .left-side -->
-								<div class="right-side-inner">
-									<h5><span>Prymb Creative Studio</span>has got a job!</h5>
-									<h5><a href="#0">We're looking for an Art Director</a>in<a href="#0">Tecnologies</a></h5>
-								</div> <!-- end .right-side -->
-							</div> <!-- end .latest-update -->
-
-						</div> <!-- end .latest-updates-widget-wrapper -->
+					
 
 					</div> <!-- end .left-side -->
 
 					<div class="right-side-wrapper">
 						<div class="right-side-top">
-							<h6><span><i class="ion-ios-arrow-left"></i></span>Back to <a href="#0">Technologies</a></h6>
+							<h6><span><i class="ion-ios-arrow-left"></i></span>Back to <a href="{{route('jobListening')}}">All Jobs</a></h6>
 							<div class="right-side-top-inner flex no-wrap">
 							
 								<div class="job-post-wrapper">
 									<div class="job-post-top flex no-column no-wrap">
 										<div class="job-post-top-left">
-											<img src="images/company-logo-big01.jpg" alt="company-logo" class="img-responsive">
+											<img src="images/{{$jobdetails->image}}" alt="company-logo" class="img-responsive">
 										</div> <!-- end .left-side-inner -->
 										<div class="job-post-top-right">
-											<h4 class="dark">Front-end developer needed</h4>
-											<h5>Banana inc.</h5>
+											<h4 class="dark">{{$jobdetails->jobName}}</h4>
+											<h5>{{$jobdetails->cname}}</h5>
 											<div class="job-post-meta flex items-center no-column no-wrap">
 												<div class="bookmarked-job-meta flex items-center no-wrap no-column">
-													<h6 class="bookmarked-job-category">Technologies</h6>
-						        					<h6 class="candidate-location">Park Ave,<span>NYC, USA</span></h6>
-													<h6 class="hourly-rate">$60<span>/Hour</span></h6>
+						        					<h6 class="candidate-location">{{$jobdetails->addresscol}},<span>{{$jobdetails->cityname}}, {{$jobdetails->statename}}</span></h6>
+													<h6 class="hourly-rate">{{"$".$jobdetails->job_amount}}<span>/Hour</span></h6>
 						        				</div>
-					        					<h6 class="full-time">Full Time</h6>
+					        					<h6 class="full-time">{{$jobdetails->typeName}}</h6>
 											</div> <!-- end .job-post-meta -->
 										</div> <!-- end .right-side-inner -->
 									</div> <!-- end .job-post-top -->
@@ -138,42 +85,27 @@
 
 									<div class="job-post-bottom">
 										<h4 class="dark">Job Description</h4>
-										<p>Etiam quis interdum felis, at pellentesque metus. Morbi eget congue lectus. Donec eleifend ultricies urna et euismod. Sed consectetur tellus eget odio aliquet, vel vestibulum tellus sollicitudin. Morbi maximus metus eu eros tincidunt, vitae mollis ante imperdiet. Nulla imperdiet at mauris ut posuere.<br><br>Nullam tempor, ipsum eget egestas viverra, velit augue fringilla arcu, et sollicitudin enim eros nec est. Suspendisse volutpat velit non porttitor placerat. Mauris porttitor aliquam bibendum. Nam at ultrices justo. Mauris eget urna magna.</p><br>	
 
-										<h4 class="dark">Challenges &amp; Benifits</h4>
-										<p>Etiam quis interdum felis, at pellentesque metus. Morbi eget congue lectus. Donec eleifend ultricies urna et euismod. Sed consectetur tellus eget odio aliquet, vel vestibulum tellus sollicitudin. Morbi maximus metus eu eros tincidunt, vitae mollis ante imperdiet. Nulla imperdiet at mauris ut posuere.</p><br>
-
-										<ul class="job-post-nested-list list-unstyled">
-											<li class="flex no-column no-wrap"><span><i class="ion-ios-checkmark"></i></span>Donec accumsan auctor iaculis. Nullam non tortor massa. Proin ligula leo, hendrerit quis tincidunt a, sodales eget ligula. Aenean et est tristique, dictum lorem vel, porttitor urna.</li>
-											<li class="flex no-column no-wrap"><span><i class="ion-ios-checkmark"></i></span>Suspendisse gravida elementum lacus, a malesuada tortor sollicitudin ut. Donec pharetra metus lectus, ut eleifend eros sollicitudin et. Ut at lobortis dolor, eget commodo tortor. Curabitur bibendum consequat neque a tincidunt. In in euismod quam. Proin in egestas eros. Cum sociis natoque penatibus et magnis dis parturient montes.</li>
-											<li class="flex no-column no-wrap"><span><i class="ion-ios-checkmark"></i></span>Etiam cursus metus arcu, ut pellentesque eros dictum vitae. Vivamus purus ex, dapibus ut eleifend in, hendrerit non nibh. Donec ornare molestie vehicula. Duis feugiat iaculis convallis.</li>
-										</ul> <!-- end .job-post-nested-list -->
-
-										<br>
-										
-										<h4 class="dark">Requirements</h4>
-										<p>Nunc nec porttitor turpis. In eu risus enim. In vitae mollis elit. Etiam a laoreet justo, laoreet blandit arcu. Vestibulum lacinia, lacus vitae dignissim tempor, mi tellus imperdiet dolor, quis consectetur tortor magna id enim.<br><br>Vestibulum ac vestibulum dui. Vivamus finibus vel mauris ut vehicula. Nullam a magna porttitor, dictum risus nec, faucibus sapien. Curabitur porttitor in est at viverra. Sed dictum felis lorem.</p>
-
-										<br>
-										
+                                        {{$jobdetails->jdetails}}
 										<div class="divider"></div>
-
-										<div class="job-post-share flex space-between items-center no-wrap">
-											<div class="job-post-share-left flex items-center no-wrap">
-												<h6>Share this job:</h6>
-												<ul class="social-share flex no-wrap no-column list-unstyled">
-													<li><a href="#0" class="button button-sm facebook-btn"><span><i class="ion-social-facebook"></i></span>Facebook</a></li>
-													<li><a href="#0" class="button button-sm twitter-btn"><span><i class="ion-social-twitter"></i></span>Twitter</a></li>
-													<li><a href="#0" class="button button-sm g-plus-btn"><span><i class="ion-social-googleplus"></i></span>Google plus</a></li>
-												</ul> <!-- end .social-share -->
-											</div> <!-- end .job-post-share-left -->
-											<div class="job-post-share-right flex items-center no-column no-wrap">
-												<h6>Bookmark this job</h6>
-												<i class="ion-ios-heart wishlist-icon"></i>
-											</div> <!-- end .job-post-share-right -->
-											
-										</div> <!-- end .job-post-share -->
-
+                                        <div class="job-post-bottom">
+                                        <h4 class="dark">Job Time</h4>
+										{{--<div class="job-post-share flex space-between items-center no-wrap">--}}
+											{{--<div class="job-post-share-left flex items-center no-wrap">--}}
+												{{--<h6>Share this job:</h6>--}}
+												{{--<ul class="social-share flex no-wrap no-column list-unstyled">--}}
+													{{--<li><a href="#0" class="button button-sm facebook-btn"><span><i class="ion-social-facebook"></i></span>Facebook</a></li>--}}
+													{{--<li><a href="#0" class="button button-sm twitter-btn"><span><i class="ion-social-twitter"></i></span>Twitter</a></li>--}}
+													{{--<li><a href="#0" class="button button-sm g-plus-btn"><span><i class="ion-social-googleplus"></i></span>Google plus</a></li>--}}
+												{{--</ul> <!-- end .social-share -->--}}
+											{{--</div> <!-- end .job-post-share-left -->--}}
+											{{--<div class="job-post-share-right flex items-center no-column no-wrap">--}}
+												{{--<h6>Bookmark this job</h6>--}}
+												{{--<i class="ion-ios-heart wishlist-icon"></i>--}}
+											{{--</div> <!-- end .job-post-share-right -->--}}
+											{{----}}
+										{{--</div> <!-- end .job-post-share -->--}}
+                                        </div>
 									</div> <!-- end .job-post-bottom -->
 
 								</div> <!-- end .left-side-inner -->
@@ -181,12 +113,12 @@
 								<div class="right-side-inner">
 									<div id="map" class="map on-job-details-page"></div>
 									<div class="job-post-company-info">
-										<h5 class="dark">Banana inc.</h5>
+										<h5 class="dark">{{$jobdetails->cname}}</h5>
 										<ul class="list-unstyled">
-											<li class="flex no-column no-wrap"><i class="ion-ios-location"></i>56/23 Park Ave, Manhattan NYC 10001, USA</li>
-											<li class="flex no-column no-wrap"><i class="ion-ios-telephone"></i><a href="tel:(+01)-212-342-6789">(+01)-212-342-6789</a></li>
-											<li class="flex no-column no-wrap"><i class="ion-email"></i><a href="mailto:hr@banana.inc">hr@banana.inc</a></li>
-											<li class="flex no-column no-wrap"><i class="ion-earth"></i><a href="#0">https://banana.inc</a></li>
+											<li class="flex no-column no-wrap"><i class="ion-ios-location"></i>{{$jobdetails->addresscol}}</li>
+											<li class="flex no-column no-wrap"><i class="ion-ios-telephone"></i><a href="tel:(+01)-212-342-6789">{{$jobdetails->phone}}</a></li>
+											<li class="flex no-column no-wrap"><i class="ion-email"></i><a href="mailto:{{$jobdetails->email}}">{{$jobdetails->email}}</a></li>
+											<li class="flex no-column no-wrap"><i class="ion-earth"></i><a target="_blank" href="http://{{$jobdetails->website}}">{{$jobdetails->website}}</a></li>
 										</ul>
 									</div> <!-- end .job-post-company-info -->
 
