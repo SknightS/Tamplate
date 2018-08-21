4cdb7f2fe80434db37d42752a4b14a61eb5d7a2e
mdsak <md.sakibrahman@hotmail.com>
2018-08-21 11:48:00 +0600

    sakib

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -44,7 +44,7 @@ class JobController extends Controller
             ->with('alljob', $alljob);
     }
 
-    public function jobdetails($postid, $typename){
+    public function jobdetails( $typename, $postid){
 
 
         $jobdetails = Post::select('*','job.id as jobid','post.id as postid','jobName', 'company_branch.name as cname','job.description as jdetails','company_branch.image as image ', 'post.description as pdes','typeName','address.addresscol as address','job.job_amount as job_amount', 'master_state.name as statename','master_subarb.name as cityname')
@@ -57,7 +57,7 @@ class JobController extends Controller
             ->where ('post.id', $postid)
             ->first();
 
-        $similarjob = $alljob = Post::select('*','job.id as jobid','post.id as postid','jobName', 'company_branch.name as cname', 'post.description as pdes','typeName','address.addresscol as address','job.job_amount as job_amount')
+        $similarjob  = Post::select('*','job.id as jobid','post.id as postid','jobName', 'company_branch.name as cname', 'post.description as pdes','typeName','address.addresscol as address','job.job_amount as job_amount')
             ->leftJoin('job', 'job.id', 'post.fkjobId')
             ->leftjoin ('company_branch','job.company_branch_id','company_branch.id')
             ->leftjoin('jobtype','job.fkjobTypeId','jobtype.id')
@@ -65,7 +65,6 @@ class JobController extends Controller
             ->leftjoin('master_subarb','address.master_subarb_id','master_subarb.id')
             ->leftjoin('master_state','master_subarb.master_state_id','master_state.id')
             ->where('typeName',$typename)
-            ->limit(3)
             ->get();
 
 
--- resources/views/layouts/job-details.blade.php
+++ resources/views/layouts/job-details.blade.php
@@ -142,64 +142,98 @@
 							<div class="right-side-bottom-wrapper">
 							
 						        <div class="bookmarked-jobs-list-wrapper on-listing-page on-job-detals-page">
-									<h3>Similar jobs from<span>Banana inc.</span></h3>
-						        	<div class="bookmarked-job-wrapper">
-						        		<div class="bookmarked-job flex no-wrap no-column ">
-							        		<div class="job-company-icon">
-							        			<img src="images/company-logo-big01.jpg" alt="company-icon" class="img-responsive">
-							        		</div> <!-- end .job-icon -->
-							        		<div class="bookmarked-job-info">
-							        			<h4 class="dark flex no-column">We're looking for a designer<a href="#0" class="button full-time">full time</a></h4>
-							        			<h5>Banana inc.</h5>
-							        			<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam eu velit cursus, tempor ipsum in, tempus lectus. Nullam tempus nisi id nisl luctus, non tempor justo molestie.</p>
-							        			<div class="bookmarked-job-info-bottom flex space-between items-center no-column no-wrap">
-							        				<div class="bookmarked-job-meta flex items-center no-wrap no-column">
-								        				<ul class="list-unstyled candidates-avatar flex items-center no-wrap no-column">
-							        						<li><img src="images/avatar02.jpg" alt="avatar" class="img-responsive"></li>
-							        						<li><img src="images/avatar03.jpg" alt="avatar" class="img-responsive"></li>
-							        						<li class="candidates-total-count"><img src="images/avatar04.jpg" alt="avatar" class="img-responsive"><span>54+</span></li>
-							        					</ul> <!-- end .candidates-avatar -->
-														<h6 class="bookmarked-job-category">Art/Design</h6>
-							        					<h6 class="candidate-location">Cupertino,<span>CA, USA</span></h6>
-														<h6 class="hourly-rate">$45<span>/Hour</span></h6>
-							        				</div> <!-- end .bookmarked-job-meta -->
-							        				<div class="right-side-bookmarked-job-meta flex items-center no-column no-wrap">
-							        					<i class="ion-ios-heart wishlist-icon"></i>
-							        					<a href="#0" class="button">more detail</a>
-							        				</div> <!-- end .right-side-bookmarked-job-meta -->
-							        			</div> <!-- end .bookmarked-job-info-bottom -->
-							        		</div> <!-- end .bookmarked-job-info -->
-						        		</div> <!-- end .bookmarked-job -->
-						        	</div> <!-- end .bookmarked-job-wrapper --> 
+									<h3>Similar jobs </h3>
 
-						        	<div class="bookmarked-job-wrapper">
-						        		<div class="bookmarked-job flex no-wrap no-column ">
-							        		<div class="job-company-icon">
-							        			<img src="images/company-logo-big01.jpg" alt="company-icon" class="img-responsive">
-							        		</div> <!-- end .job-icon -->
-							        		<div class="bookmarked-job-info">
-							        			<h4 class="dark flex no-column">We need a web designer<a href="#0" class="button part-time">Part time</a></h4>
-							        			<h5>Banana inc.</h5>
-							        			<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam eu velit cursus, tempor ipsum in, tempus lectus. Nullam tempus nisi id nisl luctus, non tempor justo molestie.</p>
-							        			<div class="bookmarked-job-info-bottom flex space-between items-center no-column no-wrap">
-							        				<div class="bookmarked-job-meta flex items-center no-wrap no-column">
-								        				<ul class="list-unstyled candidates-avatar flex items-center no-wrap no-column">
-							        						<li><img src="images/avatar02.jpg" alt="avatar" class="img-responsive"></li>
-							        						<li><img src="images/avatar03.jpg" alt="avatar" class="img-responsive"></li>
-							        						<li class="candidates-total-count"><img src="images/avatar04.jpg" alt="avatar" class="img-responsive"><span>54+</span></li>
-							        					</ul> <!-- end .candidates-avatar -->
-														<h6 class="bookmarked-job-category">Art/Design</h6>
-							        					<h6 class="candidate-location">Cupertino,<span>CA, USA</span></h6>
-														<h6 class="hourly-rate">$45<span>/Hour</span></h6>
-							        				</div> <!-- end .bookmarked-job-meta -->
-							        				<div class="right-side-bookmarked-job-meta flex items-center no-column no-wrap">
-							        					<i class="ion-ios-heart wishlist-icon"></i>
-							        					<a href="#0" class="button">more detail</a>
-							        				</div> <!-- end .right-side-bookmarked-job-meta -->
-							        			</div> <!-- end .bookmarked-job-info-bottom -->
-							        		</div> <!-- end .bookmarked-job-info -->
-						        		</div> <!-- end .bookmarked-job -->
-						        	</div> <!-- end .bookmarked-job-wrapper -->	
+									@foreach($similarjob as $sj)
+										<div class="bookmarked-jobs-list-wrapper on-listing-page">
+											<div class="bookmarked-job-wrapper">
+												<div class="bookmarked-job flex no-wrap no-column ">
+													<div class="job-company-icon">
+														<img src="images/company-logo-big01.jpg" alt="company-icon" class="img-responsive">
+													</div> <!-- end .job-icon -->
+													<div class="bookmarked-job-info">
+														<h4 class="dark flex no-column">{{$sj->jobName}}</h4>
+														<h5>{{$sj->cname}}</h5>
+														<p>{{$sj->pdes}}</p>
+														<div class="bookmarked-job-info-bottom flex space-between items-center no-column no-wrap">
+															<div class="bookmarked-job-meta flex items-center no-wrap no-column">
+																{{--<ul class="list-unstyled candidates-avatar flex items-center no-wrap no-column">--}}
+																{{--<li><img src="images/avatar02.jpg" alt="avatar" class="img-responsive"></li>--}}
+																{{--<li><img src="images/avatar03.jpg" alt="avatar" class="img-responsive"></li>--}}
+																{{--<li class="candidates-total-count"><img src="images/avatar04.jpg" alt="avatar" class="img-responsive"><span>54+</span></li>--}}
+																{{--</ul> <!-- end .candidates-avatar -->--}}
+																<h6 class="bookmarked-job-category">{{$sj->typeName}}</h6>
+																<h6 class="candidate-location">{{$sj->address}}</h6>
+																<h6 class="hourly-rate">{{$sj->job_amount}}<span>/Hour</span></h6>
+															</div> <!-- end .bookmarked-job-meta -->
+															<div class="right-side-bookmarked-job-meta flex items-center no-column no-wrap">
+																{{--<i class="ion-ios-heart wishlist-icon"></i>--}}
+																<a href="{{route('layouts.jobdetails', [ $sj->typeName, $sj->postid] )}}" class="button">more detail</a>
+															</div> <!-- end .right-side-bookmarked-job-meta -->
+														</div> <!-- end .bookmarked-job-info-bottom -->
+													</div> <!-- end .bookmarked-job-info -->
+												</div> <!-- end .bookmarked-job -->
+											</div> <!-- end .bookmarked-job-wrapper -->
+
+										</div> <!-- end .bookmarked-jobs-list-wrapper -->
+									@endforeach
+						        	{{--<div class="bookmarked-job-wrapper">--}}
+						        		{{--<div class="bookmarked-job flex no-wrap no-column ">--}}
+							        		{{--<div class="job-company-icon">--}}
+							        			{{--<img src="images/company-logo-big01.jpg" alt="company-icon" class="img-responsive">--}}
+							        		{{--</div> <!-- end .job-icon -->--}}
+							        		{{--<div class="bookmarked-job-info">--}}
+							        			{{--<h4 class="dark flex no-column">We're looking for a designer<a href="#0" class="button full-time">full time</a></h4>--}}
+							        			{{--<h5>Banana inc.</h5>--}}
+							        			{{--<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam eu velit cursus, tempor ipsum in, tempus lectus. Nullam tempus nisi id nisl luctus, non tempor justo molestie.</p>--}}
+							        			{{--<div class="bookmarked-job-info-bottom flex space-between items-center no-column no-wrap">--}}
+							        				{{--<div class="bookmarked-job-meta flex items-center no-wrap no-column">--}}
+								        				{{--<ul class="list-unstyled candidates-avatar flex items-center no-wrap no-column">--}}
+							        						{{--<li><img src="images/avatar02.jpg" alt="avatar" class="img-responsive"></li>--}}
+							        						{{--<li><img src="images/avatar03.jpg" alt="avatar" class="img-responsive"></li>--}}
+							        						{{--<li class="candidates-total-count"><img src="images/avatar04.jpg" alt="avatar" class="img-responsive"><span>54+</span></li>--}}
+							        					{{--</ul> <!-- end .candidates-avatar -->--}}
+														{{--<h6 class="bookmarked-job-category">Art/Design</h6>--}}
+							        					{{--<h6 class="candidate-location">Cupertino,<span>CA, USA</span></h6>--}}
+														{{--<h6 class="hourly-rate">$45<span>/Hour</span></h6>--}}
+							        				{{--</div> <!-- end .bookmarked-job-meta -->--}}
+							        				{{--<div class="right-side-bookmarked-job-meta flex items-center no-column no-wrap">--}}
+							        					{{--<i class="ion-ios-heart wishlist-icon"></i>--}}
+							        					{{--<a href="#0" class="button">more detail</a>--}}
+							        				{{--</div> <!-- end .right-side-bookmarked-job-meta -->--}}
+							        			{{--</div> <!-- end .bookmarked-job-info-bottom -->--}}
+							        		{{--</div> <!-- end .bookmarked-job-info -->--}}
+						        		{{--</div> <!-- end .bookmarked-job -->--}}
+						        	{{--</div> <!-- end .bookmarked-job-wrapper --> --}}
+
+						        	{{--<div class="bookmarked-job-wrapper">--}}
+						        		{{--<div class="bookmarked-job flex no-wrap no-column ">--}}
+							        		{{--<div class="job-company-icon">--}}
+							        			{{--<img src="images/company-logo-big01.jpg" alt="company-icon" class="img-responsive">--}}
+							        		{{--</div> <!-- end .job-icon -->--}}
+							        		{{--<div class="bookmarked-job-info">--}}
+							        			{{--<h4 class="dark flex no-column">We need a web designer<a href="#0" class="button part-time">Part time</a></h4>--}}
+							        			{{--<h5>Banana inc.</h5>--}}
+							        			{{--<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam eu velit cursus, tempor ipsum in, tempus lectus. Nullam tempus nisi id nisl luctus, non tempor justo molestie.</p>--}}
+							        			{{--<div class="bookmarked-job-info-bottom flex space-between items-center no-column no-wrap">--}}
+							        				{{--<div class="bookmarked-job-meta flex items-center no-wrap no-column">--}}
+								        				{{--<ul class="list-unstyled candidates-avatar flex items-center no-wrap no-column">--}}
+							        						{{--<li><img src="images/avatar02.jpg" alt="avatar" class="img-responsive"></li>--}}
+							        						{{--<li><img src="images/avatar03.jpg" alt="avatar" class="img-responsive"></li>--}}
+							        						{{--<li class="candidates-total-count"><img src="images/avatar04.jpg" alt="avatar" class="img-responsive"><span>54+</span></li>--}}
+							        					{{--</ul> <!-- end .candidates-avatar -->--}}
+														{{--<h6 class="bookmarked-job-category">Art/Design</h6>--}}
+							        					{{--<h6 class="candidate-location">Cupertino,<span>CA, USA</span></h6>--}}
+														{{--<h6 class="hourly-rate">$45<span>/Hour</span></h6>--}}
+							        				{{--</div> <!-- end .bookmarked-job-meta -->--}}
+							        				{{--<div class="right-side-bookmarked-job-meta flex items-center no-column no-wrap">--}}
+							        					{{--<i class="ion-ios-heart wishlist-icon"></i>--}}
+							        					{{--<a href="#0" class="button">more detail</a>--}}
+							        				{{--</div> <!-- end .right-side-bookmarked-job-meta -->--}}
+							        			{{--</div> <!-- end .bookmarked-job-info-bottom -->--}}
+							        		{{--</div> <!-- end .bookmarked-job-info -->--}}
+						        		{{--</div> <!-- end .bookmarked-job -->--}}
+						        	{{--</div> <!-- end .bookmarked-job-wrapper -->	--}}
 					        	</div> <!-- end .bookmarked-jobs-list-wrapper -->
 							</div> <!-- end .right-side-bottom-wrapper -->
 							</div> <!-- end .right-side-top-inner -->
